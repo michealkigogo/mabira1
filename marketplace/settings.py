ee4b4137c7f370687f09e9466ce0bf5371fc33c1
MRMIKE <michealkigogo@gmail.com>
2025-04-13 00:40:10 +0100

    Update settings.py

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -97,13 +97,15 @@ WSGI_APPLICATION = 'marketplace.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
-    DATABASES = {
-    'default': dj_database_url.config(
+   DATABASES = {
+    'default':{
+    dj_database_url.config(
         default=os.getenv('DATABASE_URL'),  
         conn_max_age=600,
         ssl_require=True  
     )
 }
+   }
 
 
 
